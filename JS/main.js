449ce814a80fe870b73177108a8964190f52a989
Gabriel Santos <gabi.santos.pu@gmail.com>
2022-12-08 13:11:10 -0300

    Finished function to call the slide and delete the first message

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,25 @@
-$('.btneyes').click(function(){
-  $('.wrapper').toggleClass('visivel');
+var button = document.querySelector(".btn-eyes");
+
+button.addEventListener('click', function () {
+  var mensagem = document.querySelector(".open-eyes");
+  var slide = document.querySelector(".wrapper");
+
+  if(slide.style.display === "none") {
+    slide.style.display = "block"
+  } else {
+    slide.style.display = "none"
+  }
+
+  if(mensagem.style.display === "block") {
+    mensagem.style.display = "none"
+  } else {
+    mensagem.style.display = "block"
+  }
+  
+slide 
 });
+
+
 
 
 
