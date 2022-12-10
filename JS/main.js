d494300fb99d7882345f99999d0357fda878ce54
Gabriel Santos <gabi.santos.pu@gmail.com>
2022-12-10 19:45:33 -0300

    Upgrade animetion style

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
+// BOTÃO PARA TRAZER SLIDE E APAGAR A MENSAGEM
+
 var button = document.querySelector(".btn-eyes");
 
 button.addEventListener('click', function () {
@@ -20,9 +22,8 @@ slide
 });
 
 
+// DESENVOLVIMENTO DO SLIDE
 
-
-let delay = -0.4;
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     grabCursor: true,
@@ -36,3 +37,16 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  // Tentativa de criação de slide pelo JS
+
+// function construcaoSlide (texto, autor) {
+//   return `
+//     <div class="wrapper">
+//       <p>${texto}</p>
+//       <cite>${autor}</cite>
+//   `
+// }
+
+// document.querySelector('#page-txt').innerHTML =
+//     construcaoSlide("<div class="box swiper-slide"> <blockquote> <p>"Outros amaram em mim a mulher<br> A ti peço para amar<br> A sombra <br>Ama-me os fios de cabelo que caem <br>O farelo à ponta dos dedos <br>A poeira das unhas recém-lixadas <br>Ama na minha boca a palavra que nunca é dita <br>Ama nos meus livros aquele que achei num sebo <br>E que decidi comprar só pela dedicatória Datada de junho de 1732 <br>Ver a caligrafia de um morto desejando boa leitura <br>A outro morto <br>Duas mãos se encontrando assim, cobertas <br>De esquecimento e pó"</p><cite class="citacao">Mar Becker</cite></blockquote></div>)
