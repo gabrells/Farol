77be3d8409ad54f2f29f2c810f61d2580253d991
Gabriel Santos <gabi.santos.pu@gmail.com>
2022-12-11 23:27:16 -0300

    Upgrade desing of page

--- JS/main.js
+++ JS/main.js
@@ -5,12 +5,18 @@ var button = document.querySelector(".btn-eyes");
 button.addEventListener('click', function () {
   var mensagem = document.querySelector(".open-eyes");
   var slide = document.querySelector(".wrapper");
+  // var bg = document.querySelector(".inicio");
 
   if(slide.style.display === "none") {
     slide.style.display = "block"
   } else {
     slide.style.display = "none"
   }
+  // if(bg.style.display === "none") {
+  //   bg.style.display = "block"
+  // } else {
+  //   slide.style.display = "none"
+  // }
 
   if(mensagem.style.display === "block") {
     mensagem.style.display = "none"
@@ -21,32 +27,24 @@ button.addEventListener('click', function () {
 slide 
 });
 
-
 // DESENVOLVIMENTO DO SLIDE
+const buttons = document.querySelectorAll('.buttons_btn')
+const comment = document.getElementById('texto')
+const events = ['click', 'keyup']
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    grabCursor: true,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
-  // Tentativa de criação de slide pelo JS
-
-// function construcaoSlide (texto, autor) {
-//   return `
-//     <div class="wrapper">
-//       <p>${texto}</p>
-//       <cite>${autor}</cite>
-//   `
-// }
-
-// document.querySelector('#page-txt').innerHTML =
-//     construcaoSlide("<div class="box swiper-slide"> <blockquote> <p>"Outros amaram em mim a mulher<br> A ti peço para amar<br> A sombra <br>Ama-me os fios de cabelo que caem <br>O farelo à ponta dos dedos <br>A poeira das unhas recém-lixadas <br>Ama na minha boca a palavra que nunca é dita <br>Ama nos meus livros aquele que achei num sebo <br>E que decidi comprar só pela dedicatória Datada de junho de 1732 <br>Ver a caligrafia de um morto desejando boa leitura <br>A outro morto <br>Duas mãos se encontrando assim, cobertas <br>De esquecimento e pó"</p><cite class="citacao">Mar Becker</cite></blockquote></div>)
+buttons.forEach(btn => {
+  events.forEach(event => {
+    btn.addEventListener(event, () => {
+      // console.log(event)
+      if (!comment.classList.contains('comment')) {
+        comment.classList.add('comment')
+        comment.innerHTML = `
+        <p>"A verdade <br>É que todos somos fogo <br>Ás vezes fagulha às vezes incêndio."
+                </p>
+                <cite><a href="https://www.instagram.com/tempofeio/" target="_blank" class="link-citacao">Tempo Feio</a></cite>
+      
+        `
+      }
+    })
+  })
+})
---
+++ README.md
@@ -0,0 +1,6 @@
+<h1 aling="center"> Playful page with slider control</h1>
+
+-- Em construção
+
+Este é um projeto autoral, onde pretendo colocar um pouco da minha graduação nele. 
+A idéia é criar uma página lúdica do zero com textos e poemas que me trazem alguma reflexão, essa pagina contará com um controle de slide e deverá trazer uma sensação de imerção ao usuario. 
